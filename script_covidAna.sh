a31549c8d639f321db400f5638bb06640e42f452
lzimp <dxlin@impcas.ac.cn>
2022-09-09 13:50:17 +0800

    updated

--- script_covidAna.sh
+++ script_covidAna.sh
@@ -1,6 +1,10 @@
 # a script to run the python macros for test
 
-python3 pymacro_nhcData2022.py
-python3 pymacro_skimDailyInfo.py
-python3 pymacro_cnCovid.py
+#python3 pymacro_nhcData2022.py
+#python3 pymacro_skimDailyInfo.py
+#python3 pymacro_cnCovid.py
 
+
+git add .
+git commit -a -m "updated"
+git push
