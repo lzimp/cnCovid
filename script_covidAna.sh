b9513b06d2574130ba6f1bf18c69ff3c42376991
lzimp <dxlin@impcas.ac.cn>
2022-09-18 09:50:31 +0800

    updated

--- script_covidAna.sh
+++ script_covidAna.sh
@@ -1,7 +1,7 @@
 # a script to run the python macros for test
 
-#python3 pymacro_nhcData2022.py
-python3 pymacro_nhcData2209.py
+python3 pymacro_nhcData2022.py
+#python3 pymacro_nhcData2209.py
 python3 pymacro_skimDailyInfo.py
 python3 pymacro_cnCovid.py
 
