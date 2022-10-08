7a440272af75d1e49788a556635e46091413e56f
lzimp <dxlin@impcas.ac.cn>
2022-10-08 10:14:20 +0800

    combining the results

--- script_covidAna.sh
+++ script_covidAna.sh
@@ -5,6 +5,7 @@ python3 pymacro_nhcData2022.py
 python3 pymacro_skimDailyInfo.py
 python3 pymacro_cnCovid.py
 
+convert -delay 90 -loop 0 nhcRes2022/*_pstvStats2207.png nhcRes2022/covid19_atcRate2022.png nhcRes2022/covid19_cn202210.gif
 
 git add .
 git commit -a -m "updated"
